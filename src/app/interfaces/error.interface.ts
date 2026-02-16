93f3196ff322a88757063a6a1d27b5f00815d886
MeghlaB <joyhalder00113355@gmail.com>
2026-02-16 23:38:53 +0600

    zod error handel function create

---
+++ src/app/interfaces/error.interface.ts
@@ -0,0 +1,11 @@
+export interface TErrorSource {
+    path: string;
+    message: string
+}
+
+export interface TErrorResponse{
+    success: boolean
+    message: string
+    errorSource?: TErrorSource[]
+    error?: unknown
+}
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -3,21 +3,58 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import status from "http-status";
+import * as z from 'zod'
+import { TErrorResponse, TErrorSource } from "../interfaces/error.interface";
+
+
+
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-    
-    if(envVars.NODE_ENV === 'development') {
+
+    if (envVars.NODE_ENV === 'development') {
         console.error('Error From Global Error Handler:', error);
     }
 
-    let statusCode: number =  status.INTERNAL_SERVER_ERROR
+    const errorSource: TErrorSource[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR
     let message: string = 'Internal Server Error';
 
-    res.status(statusCode).json({
+    /* [
+    {
+        expected: 'string',
+        code: 'invalid_type',
+        path: [ 'username' ],
+        message: 'Invalid input: expected string'
+    },
+    {
+        expected: 'number',
+        code: 'invalid_type',
+        path: [ 'xp' ],
+        message: 'Invalid input: expected number'
+    }
+    ] */
+
+    if (error instanceof z.ZodError) {
+        statusCode = status.BAD_REQUEST
+        message = "Zod Validation Error"
+
+        error.issues.forEach((issue) => {
+            errorSource.push({
+                // path: issue.path.length > 1 ? issue.path.join('=>') : issue.path[0].toString(),
+                path: issue.path.join(' '),
+                message: issue.message,
+            })
+        })
+    }
+
+    const errorResponse: TErrorResponse = {
         success: false,
         message: message,
-        error: error.message || 'An unexpected error occurred',
-    });
+        errorSource,
+        error: envVars.NODE_ENV === 'development' ? error : undefined,
+    }   
+
+    res.status(statusCode).json(errorResponse);
 
 }
