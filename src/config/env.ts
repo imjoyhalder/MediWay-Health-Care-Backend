f5edc4796b59cd71993c1c5aeb4080c7ec6d3235
MeghlaB <joyhalder00113355@gmail.com>
2026-02-19 21:27:11 +0600

    install nodemailer and ejs create SendEmail Function

--- src/config/env.ts
+++ src/config/env.ts
@@ -16,7 +16,12 @@ interface EnvConfig {
     ACCESS_TOKEN_EXPIRES_IN: string;
     REFRESH_TOKEN_EXPIRES_IN: string, 
     BETTER_AUTH_SESSION_EXPIRES_IN: string, 
-    BETTER_AUTH_SESSION_UPDATE_AGE: string
+    BETTER_AUTH_SESSION_UPDATE_AGE: string, 
+    EMAIL_SENDER_SMTP_USER: string, 
+    EMAIL_SENDER_SMTP_PASSWORD: string,
+    EMAIL_SENDER_SMTP_HOST: string,
+    EMAIL_SENDER_SMTP_PORT: string,
+    EMAIL_SENDER_SMTP_FROM: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -32,7 +37,12 @@ const loadEnvVariables = (): EnvConfig => {
         "ACCESS_TOKEN_EXPIRES_IN",
         "REFRESH_TOKEN_EXPIRES_IN", 
         "BETTER_AUTH_SESSION_EXPIRES_IN", 
-        "BETTER_AUTH_SESSION_UPDATE_AGE"
+        "BETTER_AUTH_SESSION_UPDATE_AGE",
+        "EMAIL_SENDER_SMTP_USER", 
+        "EMAIL_SENDER_SMTP_PASSWORD",
+        "EMAIL_SENDER_SMTP_HOST",
+        "EMAIL_SENDER_SMTP_PORT",
+        "EMAIL_SENDER_SMTP_FROM"
     ];
 
     for (const varName of requireVariables) {
@@ -52,7 +62,12 @@ const loadEnvVariables = (): EnvConfig => {
         ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
         REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string, 
         BETTER_AUTH_SESSION_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_EXPIRES_IN as string, 
-        BETTER_AUTH_SESSION_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_UPDATE_AGE as string
+        BETTER_AUTH_SESSION_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_UPDATE_AGE as string, 
+        EMAIL_SENDER_SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,                                                  
+        EMAIL_SENDER_SMTP_PASSWORD: process.env.EMAIL_SENDER_SMTP_PASSWORD as string,
+        EMAIL_SENDER_SMTP_HOST: process.env.EMAIL_SENDER_SMTP_HOST as string,
+        EMAIL_SENDER_SMTP_PORT: process.env.EMAIL_SENDER_SMTP_PORT as string,
+        EMAIL_SENDER_SMTP_FROM: process.env.EMAIL_SENDER_SMTP_FROM as string,
     };
 };
 
