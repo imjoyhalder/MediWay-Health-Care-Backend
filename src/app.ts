581681485c7ee7058e25fbffb4da8a0a94dbca41
MeghlaB <joyhalder00113355@gmail.com>
2026-02-21 01:53:05 +0600

    cors configuration add

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,25 @@ import cookieParser from "cookie-parser";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./app/lib/auth";
 import path from "path";
-
-
+import cors from "cors";
+import { envVars } from "./config/env";
 
 const app: Application = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(process.cwd(), `src/app/templates`));
+
+app.use(cors({
+    origin: [
+        envVars.FRONTEND_URL as string,
+        envVars.BETTER_AUTH_URL as string,
+        'http://localhost:3000',
+        'http://localhost:5000'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+}))
 
 app.use('/api/auth', toNodeHandler(auth))
 
