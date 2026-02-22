0534c97efb5fe07d26d44a28d93a5efb100953ed
MeghlaB <joyhalder00113355@gmail.com>
2026-02-22 17:01:42 +0600

    multer and cloudinary config compelete

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { cloudinaryUpload } from "./cloudinary.config";
 import multer from "multer";
@@ -6,8 +7,25 @@ import multer from "multer";
 const storage = new CloudinaryStorage({
     cloudinary: cloudinaryUpload,
     params: (req, file)=>{
-        const orginalName = file.originalname;
-        // const extesions = 
+        const originalName = file.originalname;
+        const extension = originalName.split('.').pop()?.toLocaleLowerCase(); 
+        const fileNameWithoutExt = originalName
+            .split('.')
+            .slice(0, -1)
+            .join('.')
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            .replace(/[^a-z0-9\-]/g, ''); 
+
+            const uniqueName = Math.random().toString(36).substring(2)+'-'+Date.now()+'-'+fileNameWithoutExt;
+            const folder = extension === 'pdf' ? 'pdfs' : 'images';
+
+            return {
+                folder: `mediway-healthcare/${folder}`,
+                public_id: uniqueName, 
+                resource_type: "auto", 
+                
+            }
     }
 })
 
