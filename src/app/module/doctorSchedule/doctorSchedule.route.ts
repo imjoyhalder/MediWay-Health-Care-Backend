03a099624a53eaa7a000a77a50f735babca55415
MeghlaB <joyhalder00113355@gmail.com>
2026-03-05 14:26:14 +0600

    doctor schedule api update

--- src/app/module/doctorSchedule/doctorSchedule.route.ts
+++ src/app/module/doctorSchedule/doctorSchedule.route.ts
@@ -5,9 +5,15 @@ import { Role } from "../../../generated/prisma/enums";
 import { DoctorController } from "../doctor/doctor.controller";
 
 
+
 const router = Router()
 
 router.post('/create-doctor-schedule',
     checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
     DoctorController.createDoctorZodSchema
+)
+
+router.get('/doctor-schedule', 
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.DOCTOR, Role.PATIENT),
+    DoctorController.getDoctorSchedule
 )
