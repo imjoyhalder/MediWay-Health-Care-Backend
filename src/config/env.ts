9cf77c12281eb9cb3a62f000db0dd81bad52ba3b
MeghlaB <joyhalder00113355@gmail.com>
2026-03-11 16:33:54 +0600

    stripe add for payment

--- src/config/env.ts
+++ src/config/env.ts
@@ -14,51 +14,58 @@ interface EnvConfig {
     ACCESS_TOKEN_SECRET: string;
     REFRESH_TOKEN_SECRET: string;
     ACCESS_TOKEN_EXPIRES_IN: string;
-    REFRESH_TOKEN_EXPIRES_IN: string, 
-    BETTER_AUTH_SESSION_EXPIRES_IN: string, 
-    BETTER_AUTH_SESSION_UPDATE_AGE: string, 
-    EMAIL_SENDER_SMTP_USER: string, 
+    REFRESH_TOKEN_EXPIRES_IN: string,
+    BETTER_AUTH_SESSION_EXPIRES_IN: string,
+    BETTER_AUTH_SESSION_UPDATE_AGE: string,
+    EMAIL_SENDER_SMTP_USER: string,
     EMAIL_SENDER_SMTP_PASSWORD: string,
     EMAIL_SENDER_SMTP_HOST: string,
     EMAIL_SENDER_SMTP_PORT: string,
-    EMAIL_SENDER_SMTP_FROM: string, 
+    EMAIL_SENDER_SMTP_FROM: string,
     GOOGLE_CLIENT_ID?: string,
     GOOGLE_CLIENT_SECRET?: string,
     GOOGLE_CALLBACK_URL?: string,
+    FRONTEND_URL?: string,
     CLOUDINARY: {
         CLOUDINARY_CLOUD_NAME: string,
         CLOUDINARY_API_KEY: string,
         CLOUDINARY_API_SECRET: string
     }
-    FRONTEND_URL?: string
+    STRIPE: {
+        STRIPE_SECRET_KEY: string;
+        STRIPE_WEBHOOK_SECRET: string;
+    }
+
 }
 
 const loadEnvVariables = (): EnvConfig => {
 
     const requireVariables = [
-        'NODE_ENV', 
-        'PORT', 
-        'DATABASE_URL', 
-        'BETTER_AUTH_SECRET', 
-        'BETTER_AUTH_URL', 
+        'NODE_ENV',
+        'PORT',
+        'DATABASE_URL',
+        'BETTER_AUTH_SECRET',
+        'BETTER_AUTH_URL',
         "ACCESS_TOKEN_SECRET",
         "REFRESH_TOKEN_SECRET",
         "ACCESS_TOKEN_EXPIRES_IN",
-        "REFRESH_TOKEN_EXPIRES_IN", 
-        "BETTER_AUTH_SESSION_EXPIRES_IN", 
+        "REFRESH_TOKEN_EXPIRES_IN",
+        "BETTER_AUTH_SESSION_EXPIRES_IN",
         "BETTER_AUTH_SESSION_UPDATE_AGE",
-        "EMAIL_SENDER_SMTP_USER", 
+        "EMAIL_SENDER_SMTP_USER",
         "EMAIL_SENDER_SMTP_PASSWORD",
         "EMAIL_SENDER_SMTP_HOST",
         "EMAIL_SENDER_SMTP_PORT",
-        "EMAIL_SENDER_SMTP_FROM", 
+        "EMAIL_SENDER_SMTP_FROM",
         "GOOGLE_CLIENT_ID",
         "GOOGLE_CLIENT_SECRET",
         "GOOGLE_CALLBACK_URL",
-        "FRONTEND_URL", 
+        "FRONTEND_URL",
         "CLOUDINARY_CLOUD_NAME",
         "CLOUDINARY_API_KEY",
-        "CLOUDINARY_API_SECRET"
+        "CLOUDINARY_API_SECRET",
+        "STRIPE_SECRET_KEY",
+        "STRIPE_WEBHOOK_SECRET"
     ];
 
     for (const varName of requireVariables) {
@@ -72,14 +79,14 @@ const loadEnvVariables = (): EnvConfig => {
         PORT: parseInt(process.env.PORT as string),
         DATABASE_URL: process.env.DATABASE_URL as string,
         BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
-        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string, 
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
         ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
         REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
         ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
-        REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string, 
-        BETTER_AUTH_SESSION_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_EXPIRES_IN as string, 
-        BETTER_AUTH_SESSION_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_UPDATE_AGE as string, 
-        EMAIL_SENDER_SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,                                                  
+        REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+        BETTER_AUTH_SESSION_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_EXPIRES_IN as string,
+        BETTER_AUTH_SESSION_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_UPDATE_AGE as string,
+        EMAIL_SENDER_SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,
         EMAIL_SENDER_SMTP_PASSWORD: process.env.EMAIL_SENDER_SMTP_PASSWORD as string,
         EMAIL_SENDER_SMTP_HOST: process.env.EMAIL_SENDER_SMTP_HOST as string,
         EMAIL_SENDER_SMTP_PORT: process.env.EMAIL_SENDER_SMTP_PORT as string,
@@ -87,12 +94,16 @@ const loadEnvVariables = (): EnvConfig => {
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
-        FRONTEND_URL: process.env.FRONTEND_URL, 
+        FRONTEND_URL: process.env.FRONTEND_URL,
         CLOUDINARY: {
             CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
             CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
             CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string
-        }
+        },
+        STRIPE: {
+            STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
+            STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET as string,
+        },
     };
 };
 
