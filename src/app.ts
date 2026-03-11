58ec65c672e0016e04884101ee2ff4a452a19598
MeghlaB <joyhalder00113355@gmail.com>
2026-03-11 23:52:38 +0600

    stripe config added

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import path from "path";
 import cors from "cors";
 import { envVars } from "./config/env";
 import qs from "qs";
+import { PaymentController } from "./app/module/payment/payment.controller";
 
 const app: Application = express();
 
@@ -18,12 +19,7 @@ app.set('query parser', (str: string) => qs.parse(str))
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(process.cwd(), `src/app/templates`));
 
-app.post('/webhook', express.raw({ type: 'application/json' }),
-    async (req: Request, res: Response) => {
-        console.log("Webhook received: ", req.body);
-        res.status(200).json({success: true});
-    }
-)
+app.post('/webhook', express.raw({ type: 'application/json' }), PaymentController.handleStripeWebhookEvent)
 
 app.use(cors({
     origin: [
