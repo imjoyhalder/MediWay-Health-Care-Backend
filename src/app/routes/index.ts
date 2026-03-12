a3df4a6536505cc1a2241f7c12a93a9c89602fbb
MeghlaB <joyhalder00113355@gmail.com>
2026-03-13 00:56:33 +0600

    schedules dotor-schedules admins and appointments route create

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,10 @@ import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoutes } from "../module/user/user.route";
 import { DoctorRoutes } from "../module/doctor/doctor.route";
+import { AdminRoutes } from "../module/admin/admin.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
+import { AppointmentRoutes } from "../module/appointment/appointment.route";
 
 
 const router = Router()
@@ -11,6 +15,14 @@ router.use('/auth', AuthRoutes)
 router.use('/specialties', SpecialtyRoutes)
 router.use('/users', UserRoutes)
 router.use('/doctors', DoctorRoutes)
+
+// router.use("/patients", PatientRoutes)
+router.use("/admins", AdminRoutes)
+router.use("/schedules", scheduleRoutes)
+router.use("/doctor-schedules", DoctorScheduleRoutes)
+router.use("/appointments", AppointmentRoutes)
+// router.use("/prescriptions", PrescriptionRoutes)
+// router.use("/reviews", ReviewRoutes)
 
 
 export const IndexRoutes = router
